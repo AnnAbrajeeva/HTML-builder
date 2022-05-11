ec9948de379fa720614d58150a16c4d1352bdbbe
Ann Abrajeeva <anuta040786@yandex.ru>
2022-05-11 22:42:34 +0500

    fix: task №2

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,10 +7,11 @@ const writeStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 const readText = readline.createInterface(stdin, stdout);
 readText.write('Please, write something here \n');
 readText.on('line', (text) => {
-  writeStream.write(text + '\n');
-  if(text === 'exit') {
+  if (text === 'exit') {
     readText.close();
+    return;
   }
+  writeStream.write(text + '\n');
 });
 
 readText.on('close', () => {
